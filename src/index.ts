1a5e0e861dc0bb6b2a08f0d7a3390b9a263c3e18
湫曗 <autumnsilverleaves@gmail.com>
2021-06-12 03:46:57 +0800

    赈灾更新

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
-import { ref, watchEffect } from "vue";
+import { computed, ref, watchEffect } from "vue";
 
 const createReactiveStore = <T>(fn: () => Promise<T>) => {
   const state = ref<T>();
+  const updateingPromise = ref<Promise<T>>();
+  const updateing = computed(() => {
+    return updateingPromise.value ? true : false;
+  });
+
   const updateState = async () => {
     state.value = await fn();
+    return state.value;
   };
+
+  const controller = async () => {
+    if (!updateingPromise.value) {
+      updateingPromise.value = updateState();
+    }
+    await updateingPromise.value;
+    updateingPromise.value = null;
+  };
+
   const useData = () => {
     return {
       state,
-      promise: updateState(),
+      promise: controller(),
     };
   };
-  return { useData };
+  return { useData, controller, updateing };
 };
 
 const userInfoStore = createReactiveStore(async () => {
@@ -19,6 +34,10 @@ const userInfoStore = createReactiveStore(async () => {
     name: "chougege",
     sex: "male",
   };
+});
+
+watchEffect(() => {
+  console.log(`正在更新：${userInfoStore.updateing.value}`);
 });
 
 const userInfo = userInfoStore.useData().state;
