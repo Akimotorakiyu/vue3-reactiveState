a51e6bcc43838b590a6de4421be7490613357d33
湫曗 <autumnsilverleaves@gmail.com>
2021-06-18 01:58:46 +0800

    use suggestion

--- src/passThrough.ts
+++ src/passThrough.ts
@@ -1,6 +1,10 @@
 import { provide, inject } from "vue";
 
-export const passThroughT = <T>() => {
+/**
+ * 一般是用这个
+ * @returns
+ */
+export const passThrough = <T>() => {
   const symbolKey = Symbol();
 
   const provider = (state: T) => {
@@ -17,7 +21,12 @@ export const passThroughT = <T>() => {
   };
 };
 
-export const passThroughS = <T>(state: T) => {
+/**
+ *  这个应该是用不到的
+ * @param state
+ * @returns
+ */
+export const passThroughState = <T>(state: T) => {
   const symbolKey = Symbol();
 
   const provider = () => {
