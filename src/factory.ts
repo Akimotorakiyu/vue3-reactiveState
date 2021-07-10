e0fded89ef1961bc5e68277d687dd2bb63e83984
湫曗 <autumnsilverleaves@gmail.com>
2021-07-10 13:01:16 +0800

    add watch to factory

--- src/factory.ts
+++ src/factory.ts
@@ -1,18 +1,29 @@
+import { Ref, ComputedRef } from "vue";
 import { passThrough } from "./passThrough";
 import { createReactiveStore } from "./store";
 import { createReactiveWeakStore } from "./weakStore";
-
-export const factory = <T, Args extends unknown[]>(
-  fn: (args: Args) => Promise<T>
+import { useMessageQueen } from "./message";
+export const factory = <T, Args extends any[], E = any>(
+  fn: (...args: Args) => Promise<T>,
+  watch?: {
+    messageQueen: ReturnType<typeof useMessageQueen>;
+    handler: (
+      state: Ref<T>,
+      updater: (...args: Args) => Promise<Ref<T>>,
+      updateing: ComputedRef<boolean>,
+      event: E,
+      ...args: Args
+    ) => void;
+  }
 ) => {
   const postal = passThrough<T>();
 
   const storehouse = () => {
-    return createReactiveStore(fn);
+    return createReactiveStore(fn, watch);
   };
 
   const warehouse = () => {
-    return createReactiveWeakStore(fn);
+    return createReactiveWeakStore(fn, watch);
   };
 
   return {
