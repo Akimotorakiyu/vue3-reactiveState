50d9f949e77248610cd51d424edcfe81bd1d0fe5
湫曗 <autumnsilverleaves@gmail.com>
2021-07-13 17:41:34 +0800

    remove wareHouse

--- src/factory.ts
+++ src/factory.ts
@@ -1,43 +1,34 @@
 import { Ref, ComputedRef } from "vue";
-import { createPortal } from "./Portal";
 import { createReactiveStore } from "./store";
-import { createReactiveWeakStore } from "./weakStore";
 import { createMessageQueue } from "./message";
 
-import { IReactiveStore } from "./type";
+export const createFactory = () => {
+  const messageQueen = createMessageQueue();
 
-export const createFactory = <T, Args extends any[], E = any>(
-  fn: (...args: Args) => Promise<T>,
-  watch?: {
-    messageQueen: ReturnType<typeof createMessageQueue>;
-    handler: (
+  const storehouse = <T, Args extends any[], E = any>(
+    fn: (...args: Args) => Promise<T>,
+    handler?: (
       state: Ref<T>,
       updater: (...args: Args) => Promise<Ref<T>>,
       updateing: ComputedRef<boolean>,
       event: E,
       ...args: Args
-    ) => void;
-  }
-) => {
-  const postal = createPortal<IReactiveStore<T, Args>>();
-
-  const storehouse = () => {
-    const house = createReactiveStore(fn, watch);
-    house.postal = postal;
-
-    return house;
-  };
-
-  const warehouse = () => {
-    const house = createReactiveWeakStore(fn, watch);
-    house.postal = postal;
+    ) => void
+  ) => {
+    const house = createReactiveStore(
+      fn,
+      handler
+        ? {
+            messageQueen,
+            handler,
+          }
+        : undefined
+    );
 
     return house;
   };
 
   return {
-    postal,
     storehouse,
-    warehouse,
   };
 };
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import { createMessageQueue } from "./message";
 import { createFactory } from "./factory";
 
 export { createFactory, createMessageQueue };
-const factory = createFactory(async (id: string) => {
-  return {
-    id,
-    name: id + "chougege",
-    sex: Math.random() > 0.5 ? "female" : "male",
-  };
-});
+const factory = createFactory();
 
 {
-  const userInfoStore = factory.storehouse();
+  const userInfoStore = factory.storehouse(
+    async (id: string) => {
+      return {
+        id,
+        name: id + "chougege",
+        sex: Math.random() > 0.5 ? "female" : "male",
+      };
+    },
+    (state, updater, updateing, event, ...args) => {}
+  );
 
   watchEffect(() => {
     console.log(`正在更新：${userInfoStore.updateing.value}`);
@@ -26,23 +29,5 @@ const factory = createFactory(async (id: string) => {
 
   setInterval(() => {
     userInfoStore.updater("ddd");
-  }, 2000);
-}
-
-{
-  const userInfoStore = factory.warehouse();
-
-  watchEffect(() => {
-    console.log(`正在更新：${userInfoStore.updateing.value}`);
-  });
-
-  const userInfo = userInfoStore.useData("fff").state;
-
-  watchEffect(() => {
-    console.log(`name:${userInfo.value?.name},sex:${userInfo.value?.sex}`);
-  });
-
-  setInterval(() => {
-    userInfoStore.updater("fff");
   }, 2000);
 }
